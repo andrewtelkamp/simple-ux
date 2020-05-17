b440e83fe3e493d772480e533d8a61f25e53be51
Andrew Telkamp <me@andrewtelkamp.com>
2020-05-17 16:38:08 -0600

    button added

--- src/common/buttons/Button.js
+++ src/common/buttons/Button.js
@@ -1,37 +1,87 @@
-import React from 'react';
-import { TouchableOpacity, Text } from 'react-native';
+import React, { memo } from 'react';
+import { Image, TouchableOpacity, Text, View } from 'react-native';
+import { Colors } from '../../config';
 
-export const StandardButton = ({
-  title,
+export const Button = memo(({
+  buttonColor = 'lightgray',
+  borderColor,
+  borderWidth = 3,
+  borderRadius = 5,
+  padding = 10,
+  paddingVertical,
+  paddingHorizontal,
+  iconSize = 24,
+  isFullWidth = false,
   leftIcon,
-  isFullWidth,
-  buttonStyle,
+  onPress,
   rightIcon,
-  titleStyle,
+  title,
+  titleSize = 16,
+  titleWeight = '700',
+  titleColor = Colors.BLACK,
 }) => {
+
+  const titleStyle = {
+    color: titleColor,
+    fontSize: titleSize,
+    fontWeight: titleWeight,
+    padding,
+  }
+
+  const iconStyle = {
+    height: iconSize,
+    width: iconSize,
+  }
+
+  const iconLeftStyle = {
+    paddingRight: padding,
+  }
+
+  const iconRightStyle = {
+    paddingLeft: padding,
+  }
+
+
+
+  const buttonStyle = {
+    alignItems: 'center',
+    borderRadius,
+    borderWidth,
+    borderColor,
+    backgroundColor: buttonColor,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    // width: isFullWidth ? '100%' : undefined,
+  }
+
+  if (paddingVertical || paddingHorizontal) {
+    buttonStyle.paddingVertical = paddingVertical;
+    buttonStyle.paddingHorizontal = paddingHorizontal;
+  } else {
+    buttonStyle.padding = padding;
+  }
+
+  if (isFullWidth) {
+    buttonStyle.width = '100%';
+  }
+
+
+
   return (
-    <TouchableOpacity 
-      style={[
-        styles.button,
-        buttonStyle,
-        isFullWidth && styles.fullWidth,
-      ]}
-    >
-      { !!leftIcon && <LeftIcon />}
+    <TouchableOpacity onPress={ onPress } style={ buttonStyle }>
+      <View>
+        { !!leftIcon && (
+          <Image source={ leftIcon } style={[ iconStyle, iconLeftStyle ]} />
+        )}
+      </View>
+
       <Text style={ titleStyle }>{ title }</Text>
-      { !!rightIcon && <RightIcon />}
+
+      <View>
+        { !!rightIcon && (
+          <Image source={ rightIcon } style={[ iconStyle, iconRightStyle ]} />
+        )}
+      </View>
     </TouchableOpacity>
   );
-}
-
-const styles = {
-  button: {
-    alignItems: 'center',
-    backgroundColor: 'lightgray',
-    justifyContent: 'center',
-    padding: 10,
-  },
-  fullWidth: {
-    width: '100%',
-  },
-}
+});
