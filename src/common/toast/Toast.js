52af8e3d128c48a33e83e7a761798b6fc66ab2c0
Andrew Telkamp <me@andrewtelkamp.com>
2020-05-17 16:38:14 -0600

    toast added

--- src/common/toast/Toast.js
+++ src/common/toast/Toast.js
@@ -1,12 +1,89 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { Animated, View, Text } from 'react-native';
+import PropTypes from 'prop-types';
 
-export const Toast = ({
+import { wait } from '../../utils';
 
-}) => {
-  return (
-    <View>
-      <Text>Checkbox</Text>
-    </View>
-  );
+
+export class Toast extends Component {
+  bottom = new Animated.Value(-9999);
+
+  animateIn = () => {
+    Animated.timing(this.bottom, {
+      toValue: this.props.bottomOffset,
+      duration: 300,
+      useNativeDriver: false,
+    }).start();
+  }
+
+  animateOut = () => {
+    Animated.timing(this.bottom, {
+      toValue: -1000,
+      duration: 300,
+      useNativeDriver: false,
+    }).start();
+  }
+  
+  animateToast = async () => {
+    this.animateIn()
+    await wait(this.props.visibleDuration);
+    this.animateOut();
+  };
+
+  componentDidUpdate(prevProps) {
+    if (!prevProps.isVisible && this.props.isVisible) {
+      this.animateToast();
+    } 
+  }
+
+  render() {
+    const _toastStyle = {
+      ...styles.toast,
+      ...this.props.toastStyle,
+      position: 'absolute',
+      bottom: this.bottom,
+    };
+
+    const _textStyle = {
+      ...styles.text,
+      ...this.props.textStyle,
+    }
+
+    return (
+      <Animated.View style={ _toastStyle }>
+        <Text style={ _textStyle }>{ this.props.text }</Text>
+      </Animated.View>
+    );
+  };
 };
+
+const styles = {
+  toast: {
+    alignItems: 'center',
+    backgroundColor: 'lightgray',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    position: 'relative',
+    width: '100%',
+  },
+}
+
+// Toast.propTypes = {
+//   isVisible: PropTypes.string,
+//   showDuration: PropTypes.number.isRequired,
+//   containerStyles: PropTypes.object,
+//   iconLeft: PropTypes.string,
+//   iconLeftStyle: PropTypes.,
+//   iconRightStyle,
+//   iconRight,
+//   onIconLeftPress,
+//   onIconRightPress,
+//   text,
+//   textStyles,
+// }
+
+// Toast.defaultTypes = {
+
+// }
--- src/common/toast/index.js
+++ src/common/toast/index.js
@@ -1,1 +1,9 @@
 export { Toast } from './Toast';
+
+export const showToast = ({ message }) => {
+  <Toast
+    message='Hey! I am a toast!'
+    visibleDuration={ 3000 }
+    bottomOffset={ 100 }
+  />
+}
