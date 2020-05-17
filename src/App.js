072b53443366ed61feae18f9dbe9d71d58e1c4f2
Andrew Telkamp <me@andrewtelkamp.com>
2020-05-17 16:37:42 -0600

    implemented new inputs, ux components rendering

--- src/App.js
+++ src/App.js
@@ -6,14 +6,24 @@ import {
   Input,
   PasswordInput,
   EmailInput,
+  UsernameInput,
+  Toast,
 } from './common';
+
+const favoriteBlack = require('./assets/icons/favorite-black.png');
+const chevronRightBlack = require('./assets/icons/chevronRight-black.png');
 
 const App = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [isUsernameValid, setIsUsernameValid] = useState(false);
+  const [isToastVisible, setToastVisible] = useState(false);
 
   const onButtonPress = () => console.log('Button Pressed');
+
+  const showToast = () => {
+    setToastVisible(true);
+  };
 
   const handleUsernameChange = val => {
     if (val === 'andrewtelkamp') {
@@ -26,11 +36,25 @@ const App = () => {
   }
 
   const handlePasswordChange = val => setPassword(val)
+  const handleButtonPress = () => console.log('Button Pressed!');
 
   return (
       <View style={ styles.screen }>
         <View style={ styles.inputWrap }>
           <Input
+            backgroundColor={ Colors.BLACK }
+            focusedColor={ Colors.PURPLE }
+            label='Label'
+            onValueChange={ handleUsernameChange }
+            isValidated={ isUsernameValid }
+            value={ username }
+            validatedColor={ Colors.GREEN }
+            textColor={ Colors.WHITE }
+          />
+        </View>
+
+        <View style={ styles.inputWrap }>
+          <UsernameInput
             backgroundColor={ Colors.BLACK }
             focusedColor={ Colors.PURPLE }
             label='Label'
@@ -51,19 +75,46 @@ const App = () => {
             textColor={ Colors.WHITE }
           />
         </View>
+
+          <Button
+            isFullWidth
+            leftIcon={ favoriteBlack }
+            onPress={ showToast }
+            style={ styles.button }
+            title='Button'
+            titleStyle={ styles.buttonTitle }
+          />
+
+          <Toast
+            isVisible={ isToastVisible }
+            text="Toast Here!"
+            visibleDuration={ 3000 }
+            bottomOffset={ 100 }
+          />
       </View>
   );
 };
 
 const styles = {
+  button: {
+    backgroundColor: 'blue',
+  },
+  buttonTitle: {
+    fontWeight: '800',
+  },
   inputWrap: {
+    marginBottom: 10,
+    width: '100%',
+  },
+  wrap: {
     marginBottom: 10,
     width: '100%',
   },
   screen: {
     alignItems: 'center',
     backgroundColor: Colors.BLACK,
-    flex: 1,
+    width: '100%',
+    height: '100%',
     justifyContent: 'center',
     padding: 20,
   },
